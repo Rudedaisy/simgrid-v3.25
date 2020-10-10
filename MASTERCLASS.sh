c414fe15482a5987add3a4bff710a62e034619ec
Rudedaisy <ehanson1@umbc.edu>
2020-10-10 15:08:49 -0400

    better file placement

--- MASTERCLASS.sh
+++ MASTERCLASS.sh
@@ -2,8 +2,8 @@ cd build/
 make tests
 cd ..
 
-#/mnt/1tb/simgrid-v3.25/build/examples/s4u/app-masterworkers/s4u-app-masterworkers-fun /mnt/1tb/simgrid-v3.25/examples/platforms/cluster_and_one_host.xml.IB /mnt/1tb/simgrid-v3.25/build/examples/s4u/app-masterworkers/deployment3.xml --cfg=network/model:IB --cfg=tracing:yes --cfg=tracing/msg/process:yes --cfg=surf/precision:2e-06
+#`pwd`/build/examples/s4u/app-masterworkers/s4u-app-masterworkers-fun `pwd`/examples/platforms/cluster_and_one_host.xml.IB `pwd`/build/examples/s4u/app-masterworkers/deployment3.xml --cfg=network/model:IB --cfg=tracing:yes --cfg=tracing/msg/process:yes --cfg=surf/precision:2e-06
 
-/mnt/1tb/simgrid-v3.25/build/examples/s4u/app-masterworkers/s4u-app-masterworkers-fun /mnt/1tb/simgrid-v3.25/examples/platforms/cluster_and_one_host.xml /mnt/1tb/simgrid-v3.25/build/examples/s4u/app-masterworkers/deployment3.xml --cfg=network/model:LV08 --cfg=tracing:yes --cfg=tracing/msg/process:yes --cfg=surf/precision:500e-09
+`pwd`/build/examples/s4u/app-masterworkers/s4u-app-masterworkers-fun `pwd`/examples/platforms/cluster_and_one_host.xml `pwd`/examples/s4u/app-masterworkers/deployment3.xml --cfg=network/model:LV08 --cfg=tracing:yes --cfg=tracing/msg/process:yes --cfg=surf/precision:500e-09
 
 #Rscript /mnt/1tb/simgrid-v3.25/docs/source/tuto_s4u/draw_gantt.R simgrid.trace
